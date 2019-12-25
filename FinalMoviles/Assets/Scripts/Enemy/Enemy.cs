d66a537298c5920a3bd8dbfb771a0ca5d3e66d84
theixgamer <theofritaku@gmail.com>
2019-12-25 17:20:42 -0300

    pathfinding and enemy in progress

---
+++ FinalMoviles/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private FollowRoute followRoute;
+    public float speed;
+    protected float auxSpeed;
+    public float acceletartion;
+    public float life = 100;
+    public bool inPool;
+    
+    void Start()
+    {
+        auxSpeed = speed;
+        followRoute = GetComponent<FollowRoute>();
+        followRoute.GetAgent().speed = speed;
+        followRoute.GetAgent().acceleration = acceletartion;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckFinishRouteEnemy();
+        CheckDeadEnemy();
+    }
+    public void CheckFinishRouteEnemy()
+    {
+        if (followRoute.CheckFinishRoute())
+        {
+            if (inPool)
+            {
+                //Escribir la programacion del Resiclado del pool y todo eso.
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+    public void CheckDeadEnemy()
+    {
+        if (life <= 0)
+        {
+            if (inPool)
+            {
+                //Escribir la programacion del Resiclado del pool y todo eso.
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ FinalMoviles/Assets/Scripts/Enemy/FollowRoute.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class FollowRoute : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject finishPoint;
+    private NavMeshAgent agent;
+    public int countFinishPoints;
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        finishPoint = null;
+    }
+    private void OnEnable()
+    {
+        OnFollowRoute();
+    }
+    void Start()
+    {
+        OnFollowRoute();
+    }
+    void OnFollowRoute()
+    {
+        finishPoint = null;
+        switch (Random.Range(0, countFinishPoints))
+        {
+            case 0:
+                finishPoint = GameObject.Find("FinishPoint");
+                break;
+            case 1:
+                finishPoint = GameObject.Find("FinishPoint 1");
+                break;
+            case 2:
+                finishPoint = GameObject.Find("FinishPoint 2");
+                break;
+            case 3:
+                finishPoint = GameObject.Find("FinishPoint 3");
+                break;
+            case 4:
+                finishPoint = GameObject.Find("FinishPoint 4");
+                break;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (finishPoint != null)
+        {
+            agent.SetDestination(finishPoint.transform.position);
+        }
+    }
+    public bool CheckFinishRoute()
+    {
+        if (finishPoint != null)
+        {
+            Vector3 a = finishPoint.transform.position - transform.position;
+            //Debug.Log("Magnitud: " + a.magnitude);
+            //Debug.Log("distancia para detenerse: " + agent.stoppingDistance);
+            if (a.magnitude <= agent.stoppingDistance)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public NavMeshAgent GetAgent()
+    {
+        return agent;
+    }
+}
