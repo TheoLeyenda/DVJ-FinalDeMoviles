c81113f318cfd3bba12c63fb3cc718c1996d60c5
theixgamer <theofritaku@gmail.com>
2020-01-07 15:29:00 -0300

    Colocacion de edificios terminada

--- FinalMoviles/Assets/Scripts/Constrction/Construction.cs
+++ FinalMoviles/Assets/Scripts/Constrction/Construction.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Construction : MonoBehaviour
 {
     // Start is called before the first frame update
+    public enum PivotMovement
+    {
+        None,
+        X,
+        Y,
+        Z,
+    }
     public float life;
     public float rangeOfView;
     protected bool constructed;
     public bool rotateStart;
-
+    public PivotMovement pivotMovement;
     // Esto sirve para acomodar el edificio en caso de que este no este en el piso cuando se genere
     [Header("Start Movement Construction")]
     public float valueDown;
@@ -25,24 +32,32 @@ public class Construction : MonoBehaviour
     }
     public void UpMovement()
     {
-        if (rotateStart)
+        if (pivotMovement == PivotMovement.Y)
         {
             transform.position = transform.position + new Vector3(0, valueUp, 0);
         }
-        else
+        else if (pivotMovement == PivotMovement.Z)
         {
             transform.position = transform.position + new Vector3(0, 0, valueUp);
+        }
+        else if (pivotMovement == PivotMovement.X)
+        {
+            transform.position = transform.position + new Vector3(valueUp, 0, 0);
         }
     }
     public void DownMovement()
     {
-        if (rotateStart)
+        if (pivotMovement == PivotMovement.Y)
         {
             transform.position = transform.position - new Vector3(0, valueDown, 0);
         }
-        else
+        else if (pivotMovement == PivotMovement.Z)
         {
             transform.position = transform.position - new Vector3(0, 0, valueDown);
+        }
+        else if (pivotMovement == PivotMovement.X)
+        {
+            transform.position = transform.position - new Vector3(valueDown, 0, 0);
         }
     }
 }
