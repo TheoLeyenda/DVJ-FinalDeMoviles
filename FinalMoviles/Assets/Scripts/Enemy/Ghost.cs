ca2f0d3f7fc45e70e0e30b1835e8201d67e1a31f
theixgamer <theofritaku@gmail.com>
2020-01-19 00:54:51 -0300

    Ghost TERMINADO
    (falta probar el correcto funcionamiento del Orco Curandero y del Fantasma)

--- FinalMoviles/Assets/Scripts/Enemy/Ghost.cs
+++ FinalMoviles/Assets/Scripts/Enemy/Ghost.cs
@@ -2,17 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ghost : MonoBehaviour
+public class Ghost : Enemy
 {
     // Start is called before the first frame update
-    void Start()
+    public Material trasparentMaterial;
+    public Material normalMaterial;
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    public BoxCollider boxCollider;
+    public float DelayEnable;
+    public float auxDelayEnable;
+    public float DelayDisable;
+    public float auxDelayDisable;
+    public enum StateGhost
     {
-        
+        None,
+        InEnable,
+        InDisable,
     }
-
-    // Update is called once per frame
-    void Update()
+    private StateGhost stateGhost;
+    protected override void Start()
     {
-        
+        stateGhost = StateGhost.InEnable;
+        base.Start();
+    }
+    private void OnDisable()
+    {
+        stateGhost = StateGhost.InEnable;
+    }
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        CheckStateGhost();
+    }
+    public void CheckStateGhost()
+    {
+        if (stateGhost == StateGhost.InEnable)
+        {
+            skinnedMeshRenderer.material = normalMaterial;
+            boxCollider.enabled = true;
+            CheckEnableGhost();
+        }
+        else if (stateGhost == StateGhost.InDisable)
+        {
+            skinnedMeshRenderer.material = trasparentMaterial;
+            boxCollider.enabled = false;
+            CheckDisableGhost();
+        }
+    }
+    public void CheckEnableGhost()
+    {
+        if (DelayEnable > 0 && stateGhost == StateGhost.InEnable)
+        {
+            DelayEnable = DelayEnable - Time.deltaTime;
+        }
+        else if(DelayEnable <= 0)
+        {
+            DelayEnable = auxDelayEnable;
+            stateGhost = StateGhost.InDisable;
+        }
+    }
+    public void CheckDisableGhost()
+    {
+        if (DelayDisable > 0 && stateGhost == StateGhost.InDisable)
+        {
+            DelayDisable = DelayDisable - Time.deltaTime;
+        }
+        else if (DelayDisable <= 0)
+        {
+            DelayDisable = auxDelayDisable;
+            stateGhost = StateGhost.InEnable;
+        }
     }
 }
