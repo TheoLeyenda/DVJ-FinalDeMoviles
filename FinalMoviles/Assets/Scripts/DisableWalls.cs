2c9cee3793c653c9051a0bd350d9cd253a6d13e3
theixgamer <theofritaku@gmail.com>
2020-02-22 03:51:42 -0300

    Update Level 1

---
+++ FinalMoviles/Assets/Scripts/DisableWalls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableWalls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> walls;
+    public void DisableWall()
+    {
+        for (int i = 0; i < walls.Count; i++)
+        {
+            walls[i].SetActive(false);
+        }
+    }
+}
